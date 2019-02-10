e0ef407b2e47ae37642a63a0cb8ff5f2fe34e4c2
F3313049 Flavio Risso Sakakibara <flaviosakakibara@bb.com.br>
2019-02-10 20:08:58 -0200

    Primeira versao de modelo kmean

--- code/learning.py
+++ code/learning.py
@@ -1,7 +1,6 @@
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import pandas as pd
-import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
 import string
 
 
@@ -18,21 +17,33 @@ if __name__ == '__main__':
     logFile = '/mnt/data/logs/journalctl'
     enStopWords = stopwords.words('english')
     ptStopWords = stopwords.words('portuguese')
-    table = str.maketrans('', '', string.punctuation)
     list_of_words = []
+    vectorizer = TfidfVectorizer()
 
     # print('Files\' lines ammount: ', file_len(logFile)) 5kk lines
     with open(logFile, mode='r') as file:
 
         for line in file:
             line = line.split()
-            line = [word for word in line if word.isalpha() and word not in enStopWords]
+            # removing not alpha chars and "stop words"
+            line = [word for word in line if word.isalpha()
+                    and word not in enStopWords]
+            line = str(line)
             list_of_words.append(line)
 
-    # Defining a list of words in the log file
-    # [
-    #  [],
-    #  [],
-    # ]
+    scoredLogs = vectorizer.fit_transform(list_of_words[:1000])
 
-    print(list_of_words[:10])
+    # Defining and fitting model
+    n_clusters = 2
+    model = KMeans(n_clusters=n_clusters, init='k-means++',
+                   max_iter=100, n_init=1)
+    model.fit(scoredLogs)
+
+    print("Top terms per cluster:")
+    order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+    terms = vectorizer.get_feature_names()
+    for i in range(n_clusters):
+        print("Cluster %d:" % i)
+        for ind in order_centroids[i, :10]:
+            print(' %s' % terms[ind]),
+        print
