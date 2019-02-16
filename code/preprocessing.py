1d7b3047cbd836d759141d12c0e066bbca4d7e15
F3313049 Flavio Risso Sakakibara <flaviosakakibara@bb.com.br>
2019-02-16 17:50:04 -0200

    Added parsing log function

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,26 +1,69 @@
 import json
 import ijson
 import os
+from pathlib import Path
 import tqdm
 
 
 def generateParsed(logFile, logFileParsed):
 
-    if ~ os.path.isfile(logFile):
+    if logFile.is_file():
+        if logFileParsed.is_file():
+            print("Log file already parsed... Moving on.")
+            return 0
+        else:
+            retCode, parsedLogs = logParser(logFile)
+            print(parsedLogs)
+            return 1
+    else:
         print("Log file to parse not found. Please inform a valid one")
         return -1
-    elif os.path.isfile(logFileParsed):
-        print("Log file already parsed... Moving on.")
-        return 0
-    else:
-        logParser(logFile)
-        return 1
 
+
+def logParser(logFile):
+
+    isReading = False
+    parsedLogs = []
+    parsedLog = {
+        'message': '',
+        'priority': ''
+    }
+
+    print('Starting log parsing...')
+    with open(logFile) as inFile:
+
+        parser = ijson.parse(inFile)
+
+        for event, prefix, value in parser:
+
+            print('\nevent: ', event, 'prefix: ', prefix, 'value: ', value)
+            if prefix == 'start_array':
+                continue
+            elif prefix == 'start_map':
+                isReading = True
+                continue
+            elif prefix == 'map_key':
+                continue
+            elif prefix == 'end_map':
+                isReading = False
+                parsedLogs.append(parsedLog.copy())
+            elif prefix == 'string':
+                if event == 'item.MESSAGE':
+                    parsedLog['message'] = value
+                elif event == 'item.PRIORITY':
+                    parsedLog['priority'] = value
+                else:
+                    continue
+            elif prefix == 'end_array':
+                return 0, parsedLogs
+            else:
+                return -1, prefix
 
 if __name__ == '__main__':
 
-    logFile = '/home/flaviorissosakakibara/journalctl3.json'
-    logFileParsed = '/home/flaviorissosakakibara/journalctl3-parsed.json'
-    logFileWithoutUnwanted = '/home/flaviorissosakakibara/journalctl3-wthout.json'
+    prefixLogFile = '/home/flaviorissosakakibara/journalctl_teste'
+    logFile = Path(prefixLogFile + '.json')
+    logFileParsed = Path(prefixLogFile + '-parsed.json')
+    logFileWithoutUnwanted = Path(prefixLogFile + '-wthout.json')
 
     generateParsed(logFile, logFileParsed)
