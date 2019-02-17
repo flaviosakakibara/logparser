417ea7167997d6c5aad113c0ae5b9d55fe843992
F3313049 Flavio Risso Sakakibara <flaviosakakibara@bb.com.br>
2019-02-17 14:11:42 -0300

    Implementation of top level function process log file and some error handling

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,5 +1,6 @@
 import json
 import ijson
+import multiprocessing as mp
 from nltk.corpus import stopwords
 import os
 from pathlib import Path
@@ -15,14 +16,14 @@ def fileLen(file):
     return i + 1
 
 
-def generateParsed(logFile, logFileParsed, lineAmmount):
+def generateParsed(logFile, logFileParsed):
 
     if logFile.is_file():
         if logFileParsed.is_file():
             print("Log file already parsed... Moving on.")
             return 0
         else:
-            retCode, parsedLogs = logParser(logFile, lineAmmount)
+            retCode, parsedLogs = logParser(logFile)
             with open(logFileParsed, 'w') as outfile:
                 json.dump(parsedLogs, outfile)
             return 1
@@ -42,7 +43,7 @@ def generateWithoutUnwanted(logFileParsed, logFileWithoutUnwanted):
             json.dump(withoutUnwantedLogs, outfile)
 
 
-def logParser(logFile, lineAmmount):
+def logParser(logFile):
 
     en_stopwords = stopwords.words('english')
     isReading = False
@@ -52,82 +53,117 @@ def logParser(logFile, lineAmmount):
         'message': '',
         'priority': ''
     }
-    logFileLen = fileLen(logFile)
 
     print('Starting log parsing...')
     with open(logFile) as inFile:
 
-        # defining progress bar
-        with tqdm(
-                  #   bar_format="{postfix[0]} {postfix[1][value]:>8.10g}/{postfix[1][total]} {percentage:3.0f}%",
-                  postfix=["Logs", dict(value=0)]
-                  ) as t:
-            parser = ijson.parse(inFile)
-            for event, prefix, value in tqdm(parser):
-                numParsedLogs += 1
-                t.postfix[1]['value'] = numParsedLogs
-                t.update()
-                if prefix == 'start_array':
-                    continue
-                elif prefix == 'start_map':
-                    isReading = True
-                    continue
-                elif prefix == 'map_key':
-                    continue
-                elif prefix == 'number':
-                    continue
-                elif prefix == 'end_array':
-                    continue
-                elif prefix == 'null':
-                    continue
-                elif prefix == 'end_map':
-                    isReading = False
+        try:
+            # defining progress bar
+            with tqdm(
+                    postfix=["Logs", dict(value=0)]
+                    ) as t:
+                parser = ijson.parse(inFile)
+                for event, prefix, value in tqdm(parser):
+                    # updating progress bar
                     numParsedLogs += 1
-                    parsedLogs.append(parsedLog.copy())
-                    if numParsedLogs > lineAmmount:
-                        return 2
-                elif prefix == 'string':
-                    if event == 'item.MESSAGE':
-                        value = value.split()
-                        value = [word
-                                 for word in value
-                                 if word not in en_stopwords]
-                        value = ' '.join(value)
-                        parsedLog['message'] = value
-                    elif event == 'item.PRIORITY':
-                        parsedLog['priority'] = value
-                    else:
+                    t.postfix[1]['value'] = numParsedLogs
+                    t.update()
+                    if prefix == 'start_array':
                         continue
-                else:
-                    print('Encountered an error while parsing..')
-                    print('\nWrong prefix: ', prefix)
-                    return -1, prefix
+                    elif prefix == 'map_key':
+                        continue
+                    elif prefix == 'number':
+                        continue
+                    elif prefix == 'end_array':
+                        continue
+                    elif prefix == 'null':
+                        continue
+                    elif prefix == 'start_map':
+                        isReading = True
+                        continue
+                    elif prefix == 'end_map':
+                        isReading = False
+                        parsedLogs.append(parsedLog.copy())
+                        parsedLog = {
+                            'message': '',
+                            'priority': ''
+                        }
+                    elif prefix == 'string':
+                        if event == 'item.MESSAGE':
+                            value = value.split()
+                            value = [word
+                                     for word in value
+                                     if word not in en_stopwords]
+                            value = ' '.join(value)
+                            parsedLog['message'] = value
+                        elif event == 'item.PRIORITY':
+                            parsedLog['priority'] = value
+                        else:
+                            continue
+                    else:
+                        print('Encountered an error while parsing..')
+                        print('\nWrong prefix: ', prefix)
+                        return -1, prefix
+                return 1, parsedLogs
+        except TypeError:
+            print('\nAn error occurred...',
+                  '\n\tPrefix: ', prefix,
+                  '\n\tEvent: ', event,
+                  '\n\tValue: ', value,
+                  '\n\tParsedLog: ',
+                  parsedLog['message'],
+                  parsedLog['priority'])
+        except KeyboardInterrupt:
+            print('\nBye')
+
+
+def processLogFile(prefixLogFile):
+    # Top level funcion that coordinates other preprocessing activities
+
+    logFile = Path(prefixLogFile + '.json')
+    logFileParsed = Path(prefixLogFile + '-parsed.json')
+    logFileWithoutUnwanted = Path(prefixLogFile + '-wthout.json')
+    retcode = generateParsed(logFile, logFileParsed)
+    if retcode == -1:
+        print('An error occurred!')
+        raise
+    generateWithoutUnwanted(logFileParsed, logFileWithoutUnwanted)
 
 
 def removeUnwantedLogs(logFileParsed):
 
+    print('\nStarting unwanted logs removal...')
     unwantedMessages = [
         'i2c_hid i2c-ELAN1010:00: i2c_hid_get_input: incomplete report (14/65535)'
     ]
     with open(logFileParsed, 'r') as infile:
 
         parsedLogs = json.load(infile)
-        logsWithoutUnwanted = [log
-                               for log in parsedLogs
-                               if log['message'] not in unwantedMessages]
+        try:
+            logsWithoutUnwanted = [log
+                                   for log in parsedLogs
+                                   if log['message'] not in unwantedMessages]
+        except TypeError:
+            print('Log: ', log)
 
     return 1, logsWithoutUnwanted
 
 
 if __name__ == '__main__':
 
-    prefixLogFile = '/home/flaviorissosakakibara/journalctl3'
-    logFile = Path(prefixLogFile + '.json')
-    logFileParsed = Path(prefixLogFile + '-parsed.json')
-    logFileWithoutUnwanted = Path(prefixLogFile + '-wthout.json')
+    prefixLogFile = '/home/flaviorissosakakibara/journalctl3_1'
+    # processLogFile(prefixLogFile)
+    p1 = mp.Process(target=processLogFile,
+                    args=(prefixLogFile,),
+                    name='Process1')
 
-    retcode, parsedLogs = generateParsed(logFile, logFileParsed)
-    if retcode == -1:
-        print('An error occurred!')
-        raise
-    generateWithoutUnwanted(logFileParsed, logFileWithoutUnwanted)
+    prefixLogFile = '/home/flaviorissosakakibara/journalctl3_2'
+    # processLogFile(prefixLogFile)
+    p2 = mp.Process(target=processLogFile,
+                    args=(prefixLogFile,),
+                    name='Process2')
+
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
