da633946b22172d5e9a0b447144ea7584ab415ce
F3313049 Flavio Risso Sakakibara <flaviosakakibara@bb.com.br>
2019-02-18 20:22:38 -0300

    Removed unwanted patterns (IPs..)

--- code/tensorflow_classification.py
+++ code/tensorflow_classification.py
@@ -6,39 +6,82 @@ from tensorflow import keras
 
 # Helper libraries
 import json
+from math import floor, ceil
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
+import preprocessing
+import re
+
+
+def removeUnwantedPatterns(listOfWords):
+
+    listOfPatterns = [
+        # IPV4
+        '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
+        # Localhost IPV6
+        '^::1',
+        ''
+    ]
+
+    for pattern in listOfPatterns:
+
+        pat = re.compile(pattern)
+        listOfWords = [word
+                       for word in listOfWords
+                       if not pat.match(word)]
+
+    return listOfWords
 
 if __name__ == '__main__':
 
     class_names = ['error', 'info']
     priorities = {
-        '0': emerg
-        '1': alert
-        '2': crit
-        '3': err
-        '4': warning
-        '5': notice
-        '6': info
-        '7': debug
+        '0': 'emerg',
+        '1': 'alert',
+        '2': 'crit',
+        '3': 'err',
+        '4': 'warning',
+        '5': 'notice',
+        '6': 'info',
+        '7': 'debug'
     }
     errorPriorities = ['0', '1', '2', '3', '4']
+    ratio = {
+        'learning': 0.7,
+        'testing': 0.3
+    }
 
-    prefixLogFile = '/home/flaviorissosakakibara/journalctl3'
-    logFile = Path(prefixLogFile + '.json')
-    logFileParsed = Path(prefixLogFile + '-parsed.json')
+    prefixLogFile = '/home/flaviorissosakakibara/journalctl3_1'
     logFileWithoutUnwanted = Path(prefixLogFile + '-wthout.json')
 
     print('\nStage 1: Preprocessing')
     print('\n\t Parsing...')
-    retcode, parsedLogs = preprocessing.generateParsed(logFile, logFileParsed, 2891532)
-    if retcode == -1:
-        print('An error occurred!')
-        raise
-    print('\n\t Cleaning...')
-    preprocessing.generateWithoutUnwanted(logFileParsed, logFileWithoutUnwanted)
+    retcode = preprocessing.processLogFile(prefixLogFile)
 
+    print('\nStage 2: Data preparation')
     # Data exploration
     with open(logFileWithoutUnwanted, 'r') as infile:
         fullDataset = json.load(infile)
-    print('Size of dataset: ', len(fullDataset))
+    fullDatasetSize = len(fullDataset)
+    learningDatasetSize = ceil(fullDatasetSize * ratio['learning'])
+    testingDatasetSize = floor(fullDatasetSize * ratio['testing'])
+    print('Size of dataset: ', fullDatasetSize,
+          '\nLearning dataset size: ', learningDatasetSize,
+          '\nTesting dataset size: ', testingDatasetSize)
+    learningDataset = fullDataset.copy()[:learningDatasetSize]
+    testingDataset = fullDataset.copy()[:learningDatasetSize-1:-1]
+
+    # Extracting the messages from the dataset
+    # and spliting them into words
+    onlyMessages = [message
+                    for log in fullDataset
+                    for message in log['message'].split(' ')
+                    ]
+    onlyMessages = removeUnwantedPatterns(onlyMessages)
+    listOfDistinctWords = set(onlyMessages)
+    print('Messages: ', len(onlyMessages),
+          ' Distinct: ', len(listOfDistinctWords))
+    
+    for index, word in enumerate(listOfDistinctWords):
+        print('Index: ', index, ' Word: ', word)
