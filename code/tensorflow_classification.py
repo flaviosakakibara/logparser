79b8f12e1eec242d065752d532650ffecc735530
F3313049 Flavio Risso Sakakibara <flaviosakakibara@bb.com.br>
2019-02-18 21:45:42 -0300

    Added keras model definition

--- code/tensorflow_classification.py
+++ code/tensorflow_classification.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 # Helper libraries
+from collections import Counter
 import json
 from math import floor, ceil
 import matplotlib.pyplot as plt
@@ -20,8 +21,7 @@ def removeUnwantedPatterns(listOfWords):
         # IPV4
         '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
         # Localhost IPV6
-        '^::1',
-        ''
+        '^::1'
     ]
 
     for pattern in listOfPatterns:
@@ -32,6 +32,24 @@ def removeUnwantedPatterns(listOfWords):
                        if not pat.match(word)]
 
     return listOfWords
+
+
+def encodeMessage(message, wordDictionary):
+
+    encodedList = [encodeWord(word, wordDictionary)
+                   for word in message.split(' ')]
+
+    return encodedList
+
+
+def encodeWord(word, wordDictionary):
+
+    try:
+        encodedWord = wordDictionary[word]
+        return encodedWord
+    except KeyError:
+        return 0
+
 
 if __name__ == '__main__':
 
@@ -82,6 +100,65 @@ if __name__ == '__main__':
     listOfDistinctWords = set(onlyMessages)
     print('Messages: ', len(onlyMessages),
           ' Distinct: ', len(listOfDistinctWords))
-    
-    for index, word in enumerate(listOfDistinctWords):
-        print('Index: ', index, ' Word: ', word)
+
+    # Creating the word dictionary
+    wordDictionary = {word: index
+                      for index, word in enumerate(listOfDistinctWords, 2)}
+    wordDictionary['UNK'] = 0
+    wordDictionary['PAD'] = 1
+    wordNumber = len(wordDictionary)
+
+    # Encoding Learning and Testing datasets
+    encodedLearningDs = [{'priority': item['priority'],
+                         'message': encodeMessage(item['message'],
+                                                  wordDictionary)}
+                         for item in learningDataset]
+    encodedTestingDs = [{'priority': item['priority'],
+                        'message': encodeMessage(item['message'],
+                                                 wordDictionary)}
+                        for item in testingDataset]
+
+    maxLearningMessage = max([len(item['message'])
+                              for item in learningDataset])
+    maxTestingMessage = max([len(item['message'])
+                             for item in testingDataset])
+
+    print(encodedLearningDs[:10])
+    print(encodedTestingDs[:10])
+
+    encLearningDsMessages = [item['message']
+                             for item in encodedLearningDs]
+    encLearningDsPriority = [item['priority']
+                             for item in encodedLearningDs]
+    encTestingDsMessages = [item['message']
+                            for item in encodedTestingDs]
+    encTestingDsPriority = [item['priority']
+                            for item in encodedTestingDs]
+
+    print('Max learning message: ', maxLearningMessage)  # 798
+    print('Max testing message: ', maxTestingMessage)  # 991
+    # print(Counter([len(item['message']) for item in learningDataset]))
+    # using a max size of 1000
+    learningData = keras.preprocessing.sequence.pad_sequences(encLearningDsMessages,
+                                                              value=wordDictionary['PAD'],
+                                                              padding='post',
+                                                              maxlen=1000)
+    print(learningData[0], learningData[1])
+
+    # Defining model
+    # Labels: 0 or 1 i.e error or not error
+
+    model = keras.Sequential()
+    model.add(keras.layers.Embedding(wordNumber, 16))
+    model.add(keras.layers.GlobalAveragePooling1D())
+    model.add(keras.layers.Dense(16, activation=tf.nn.relu))
+    model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    # Generating validation data
+    x_val = 
