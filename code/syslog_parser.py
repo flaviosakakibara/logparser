d8b3273fa3f82e60983bdf6eb129e060e96f2580
F3313049 Flavio Risso Sakakibara <flaviosakakibara@bb.com.br>
2019-02-21 09:50:35 -0300

    Finished functional version of syslog_parser.

---
+++ code/syslog_parser.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import json
+from os import listdir
+
+if __name__ == '__main__':
+
+    listOfFiles = []
+    rootFolder = '/mnt/data/logs/'
+    listOfFolders = [
+                    '/mnt/data/logs/logs_nao_balanceados',
+                    '/mnt/data/logs/logs_balanceados',
+                     ]
+
+    for folder in listOfFolders:
+        for file in listdir(folder):
+            listOfFiles.append(folder+'/'+file)
+
+    # Parsing single file with multiple dicts into
+    # single file with a list of dict.
+    for folder in listOfFolders:
+        print('Folder: ', folder)
+        parsedFolder = Path(folder+'/parsed')
+        for log in listdir(folder):
+            logFile = Path(folder+'/'+log)
+            if logFile.is_file():
+                parsedLogFile = Path(folder+'/parsed/'+log)
+                print('File: ', logFile)
+                print('Parsed: ', parsedLogFile)
+                array = []
+                with open(logFile, 'r') as inFile:
+                    for line in inFile:
+                        try:
+                            array.append(eval(line))
+                        except NameError:
+                            continue
+                print('Len of parsed: ', len(array))
+                with open(parsedLogFile, 'w') as outFile:
+                    json.dump(array, outFile)
+            else:
+                continue
+        consolidatedLog = Path(folder+'/consolidated.json')
+        consolidated = []
+        for log in listdir(parsedFolder):
+            parsedLogFile = Path(folder+'/parsed/'+log)
+            with open(parsedLogFile, 'r') as inFile:
+                consolidated.extend(json.load(inFile))
+        print('Consolidated: ', len(consolidated))
+        with open(consolidatedLog, 'w') as outFile:
+            json.dump(consolidated, outFile)
